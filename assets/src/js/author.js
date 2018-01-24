117fc0cc91ae0237d9a02194adc1ff60ee616d0d
laras126 <laras126@gmail.com>
2018-01-24 09:17:50 -0800

    Author JS module

---
+++ assets/src/js/author.js
@@ -0,0 +1,15 @@
+export const $card = {
+	expandBtn: document.querySelector( '*[rel=js-card-expand-btn]' ),
+	desc: document.querySelector( '*[rel=js-card-desc]' )
+};
+
+export function init() {
+	$card.expandBtn.addEventListener( 'click', handleBioToggle );
+}
+
+// NOTE: This could be reused and abstracted as a helper function
+// to account for the "# New Stories" toggle
+export function handleBioToggle( el ) {
+	$card.desc.classList.toggle( 'js-collapsed' );
+}
+
